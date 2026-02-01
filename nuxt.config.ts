d4137638e5c578d84a7368053a3cc88679d11a0f
ybc <10533897+ybcybc@user.noreply.gitee.com>
2026-02-01 22:05:36 +0800

    feat:服务不够，暂时关闭ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
+  ssr:false,
   app: {
     baseURL: '/blog/',   // 开发、生产统一用 /blog/
   },
